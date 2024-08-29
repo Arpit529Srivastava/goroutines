305abc2124d8d4e1647d3825778d61037d6150b3
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-08-29 15:38:34 +0530

    explaining rhe use of Mutex package in mutiple goroutines

--- race.go
+++ race.go
@@ -7,25 +7,37 @@ import (
 
 func main() {
 
-	wg:=&sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
 	var scores = []int{0}
 	wg.Add(3)
-	go func(wg *sync.WaitGroup) {
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+
 		fmt.Println("this is the first goroutine")
-		scores=append(scores, 1)
-		wg.Done()
-	}(wg)
-	go func(wg *sync.WaitGroup) {
-		fmt.Println("this is the second goroutine")
-		scores=append(scores, 2)
-		wg.Done()
-	}(wg)
-	go func(wg *sync.WaitGroup) {
-		fmt.Println("this is the third goroutine")
-		scores=append(scores, 3)
+		mut.Lock()
+		scores = append(scores, 1)
+		mut.Unlock()
 		wg.Done()
 
-	}(wg)
-wg.Wait()
-fmt.Println(scores)
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+
+		fmt.Println("this is the second goroutine")
+		mut.Lock()
+		scores = append(scores, 2)
+		mut.Unlock()
+		wg.Done()
+
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+
+		fmt.Println("this is the third goroutine")
+		mut.Lock()
+		scores = append(scores, 3)
+		mut.Unlock()
+		wg.Done()
+
+	}(wg, mut)
+	wg.Wait()
+	fmt.Println(scores)
 }
