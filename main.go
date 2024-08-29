266a486ab0927f0d57b05fdc25458fdfb5d53d0e
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-08-29 13:28:43 +0530

    added a time variable for measuring the time

--- main.go
+++ main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup //waitgroups are pointers
+var wg sync.WaitGroup
+
 func Greeter(s string) {
-	defer wg.Done() //decrese the counter by one also indicates that the goroutine has fone his work
+	defer wg.Done() // Decrease the counter by one when the goroutine is done
 	result, err := http.Get(s)
 	if err != nil {
-		fmt.Println("this is not somthing i want change it")
+		fmt.Println("this is not something I want, change it")
 	} else {
-		fmt.Println("Hell yeah...status code is", result.StatusCode, s)
+		fmt.Println("Hell yeah... status code is", result.StatusCode, s)
 	}
-
 }
+
 func main() {
+	start := time.Now()
 	sites := []string{
 		"https://github.com",
 		"https://linkedin.com",
@@ -25,8 +28,10 @@ func main() {
 		"https://apple.com",
 	}
 	for _, i := range sites {
-		go Greeter(i)
-		wg.Add(1)//increment the count by n=1, indicates that there is one more goroutine waiting
+		wg.Add(1) // Increment the count before starting the goroutine
+		go Greeter(i) // Launch the Greeter function as a goroutine
 	}
-	wg.Wait()//blocks the main method until the waitgrp reaches zero
+	wg.Wait() // Blocks the main method until the waitgroup counter reaches zero
+	timeElapsed := time.Since(start)
+	fmt.Println("This for loop took this much time with goroutines:", timeElapsed)
 }
